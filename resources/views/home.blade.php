2114cfc8116ddca867216627360ffe6d0506b40a
Brayansmithe <tchoffobrayan0@gmail.com>
2023-12-19 20:07:20 +0100

    ajout de la page d accueil

---
+++ resources/views/home.blade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('layouts/css/style.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body class="body container-fuide">
+    <div class=" text-center col-md ml-5">
+        <div class="row p-0">
+            <div class="col text-start logo">
+            
+                <img src="layouts/svgs/solid/atom.svg" alt="CTM" height="30px" width="30px" class="logo">
+                <br>
+                <h3 class="text-white">  Bienvenu~~</h3>
+
+            </div>
+
+            <div class="col titre">
+                <h1 class="text-white">le titre</h1>
+            </div>
+        
+        </div>
+
+        <div class="card card1 " style="width: 20rem;height: 250px;">
+            <div class="card-body">
+            
+              <p class="card-text text-white"> quae aspernatur voluptatem, corrupti aut doloribus eius delectus accusamus atque! Sed iure a saepe cupiditate sunt! Dicta repellendus officiis dignissimos! text to build on the card title and make up the bulk of the card's content.</p>
+        
+            </div>
+          </div>
+
+          <div class="card card2 mt-2" style="width: 20rem;height: 250px;">
+                <div class="card-body">
+        
+                <p class="card-text text-white"> quae aspernatur voluptatem, corrupti aut doloribus eius delectus accusamus atque! Sed iure a saepe cupiditate sunt! Dicta repellendus officiis dignissimos! text to build on the card title and make up the bulk of the card's content.</p>
+        
+                </div>
+          </div>
+
+
+    </div>
+
+    <h3 class="text-white text-center"  style="width: 20rem;"><u>liste des utilisateurs recent</u> </h3>
+
+   <div class="container-fluide">
+    <table class="table .table-hover mt-2" style="width: 20rem;">
+     
+      <thead>
+        
+        <tr>
+          
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+        
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>bg</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Mark</td>
+          <td>bg</td>
+        </tr>
+      
+      
+      </tbody>
+    </table>
+
+
+
+
+    
+    <div class="container-fluide mt-2 nave ">
+      <ul class="nav nav-tabs" id="myTab" role="tablist">
+      
+        <li class="nav-item " role="presentation">
+          <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false"><img src="layouts/svgs/solid/house-user.svg" alt="CTM" height="35px" width="35px" class="logo"></button>
+        </li>
+        <li class="nav-item icon" role="presentation">
+          <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false"><img src="layouts/svgs/solid/toolbox.svg" alt="CTM" height="35px" width="35px" class="logo"></button>
+        </li>
+        <li class="nav-item icon" role="presentation">
+          <button class="nav-link" id="perso-tab" data-bs-toggle="tab" data-bs-target="#perso-tab-pane" type="button" role="tab" aria-controls="perso-tab-pane" aria-selected="false"><img src="layouts/svgs/solid/user.svg" alt="CTM" height="35px" width="35px" class="logo"></button>
+        </li>
+      </ul>
+      <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">...</div>
+        <div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">...</div>
+        <div class="tab-pane fade" id="perso-tab-pane" role="tabpanel" aria-labelledby="perso-tab" tabindex="0">...</div>
+
+      </div>
+
+   </div>
+   
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+
+</body>
+</html>
